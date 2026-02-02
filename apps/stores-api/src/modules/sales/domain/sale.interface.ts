94f0d666be9b2fce119a5d834c35b637a5f80b34
Andres Saray <andres.saray@dualbootpartners.com>
2026-02-01 21:20:40 -0500

    fix: fixed build

--- apps/stores-api/src/modules/sales/domain/sale.interface.ts
+++ apps/stores-api/src/modules/sales/domain/sale.interface.ts
@@ -1,4 +1,4 @@
-import { CreateSaleBody, Sale, SaleStatus } from '@shared/types/sale.types';
+import { CreateSaleBody, Sale } from '@shared/types/sale.types';
 
 export interface SaleRepository {
   createSale(body: CreateSaleBody): Promise<Sale>;
