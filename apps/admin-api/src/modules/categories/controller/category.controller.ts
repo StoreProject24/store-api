aa9e3df4271d5556241e982f33d722d8592b6de8
Andres Saray <andres.saray@dualbootpartners.com>
2026-02-02 11:50:19 -0500

    chore: enable upload images in category

--- apps/admin-api/src/modules/categories/controller/category.controller.ts
+++ apps/admin-api/src/modules/categories/controller/category.controller.ts
@@ -6,6 +6,7 @@ import {
   validatorCreateCategory,
   validatorDeleteCategory,
   validatorGetCategories,
+  validatorImageCategory,
   // validatorImageCategory,
   validatorUpdateCategory,
 } from '../validator/category.validator';
@@ -55,36 +56,36 @@ CategoriesController.patch(
   }
 );
 
-// CategoriesController.post('/images/:storeId', verifyTokenAdminStore, async (req: Request, res: Response) => {
-//   try {
-//     const categoriesDomain = new CategoryDomain();
-//     const storeId = parseInt(req.params.storeId);
-//     const images = await categoriesDomain.uploadImageCategory(storeId, req);
-//     handleSuccess(res, 200, { images });
-//   } catch (error: any) {
-//     return handleError(res, error.status, error.message);
-//   }
-// });
+CategoriesController.post('/images/:storeId', verifyTokenAdminStore, async (req: Request, res: Response) => {
+  try {
+    const categoriesDomain = new CategoryDomain();
+    const storeId = parseInt(req.params.storeId);
+    const images = await categoriesDomain.uploadImageCategory(storeId, req);
+    handleSuccess(res, HttpCode.CREATED, { images });
+  } catch (error: any) {
+    return handleError(res, error.status, error.message);
+  }
+});
 
-// CategoriesController.patch(
-//   '/images/:storeId/:id',
-//   [verifyTokenAdminStore, ...validatorImageCategory],
-//   async (req: Request, res: Response) => {
-//     try {
-//       const categoriesDomain = new CategoryDomain();
-//       const id = parseInt(req.params.id);
-//       const storeId = parseInt(req.params.storeId);
-//       const category = await categoriesDomain.updateImageCategory({
-//         categoryId: id,
-//         storeId,
-//         urlImage: req.body.urlImage,
-//       });
-//       return handleSuccess(res, 200, { category });
-//     } catch (error: any) {
-//       return handleError(res, error.status, error.message);
-//     }
-//   }
-// );
+CategoriesController.patch(
+  '/images/:storeId/:id',
+  [verifyTokenAdminStore, ...validatorImageCategory],
+  async (req: Request, res: Response) => {
+    try {
+      const categoriesDomain = new CategoryDomain();
+      const id = parseInt(req.params.id);
+      const storeId = parseInt(req.params.storeId);
+      const category = await categoriesDomain.updateImageCategory({
+        categoryId: id,
+        storeId,
+        urlImage: req.body.urlImage,
+      });
+      return handleSuccess(res, HttpCode.OK, { category });
+    } catch (error: any) {
+      return handleError(res, error.status, error.message);
+    }
+  }
+);
 
 CategoriesController.delete(
   '/:storeId/:id',
