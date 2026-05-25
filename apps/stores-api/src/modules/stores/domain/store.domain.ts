5916b3313f8bb4f778b0bd39282edf9710626a96
Andres Saray <andres.saray@dualbootpartners.com>
2026-05-25 12:25:53 -0500

    fix: fixed add signed images

--- apps/stores-api/src/modules/stores/domain/store.domain.ts
+++ apps/stores-api/src/modules/stores/domain/store.domain.ts
@@ -2,6 +2,7 @@ import { Store } from '@shared/types/store.types';
 import { getStoreById } from '../repository/store.repository';
 import { StoreRepository } from './store.interface';
 import { AppError } from '@shared/helpers/response/response';
+import { getSignedImageUrls } from '~services/image/image.service';
 
 export class StoreDomain implements StoreRepository {
   async getStore(storeId: number): Promise<Store> {
@@ -9,6 +10,12 @@ export class StoreDomain implements StoreRepository {
     if (!store) {
       throw new AppError(404, 'Store not found');
     }
-    return store;
+    const [logoUrl, bannerUrl] = await getSignedImageUrls([store.logoUrl, store.bannerUrl])
+
+    return {
+      ...store,
+      logoUrl,
+      bannerUrl
+    }
   }
 }
